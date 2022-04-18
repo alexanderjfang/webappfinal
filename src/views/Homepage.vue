5f42767cd976a1bb4fda0e20a99f0866aae2ce7f
BenBurkholder <benjamin.r.burkholder@gmail.com>
2022-04-17 22:55:01 -0400

    added menubar across the top

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,10 +1,40 @@
 <template>
-  <div class="home">
-    <h1>Welcome to Homepage</h1>
-    <button @click="homepage">Home Page</button>
-    <button @click="login">Login Page</button>
-    <button @click="allrecipes">All Recipes Page</button>
-    <button @click="favorites">Favorite Recipes Page</button>
+  <div>
+    <div class="navbar">
+      <a @click="homepage">Home</a>      
+      <a @click="allrecipes">All Recipes</a>
+      <a @click="favorites">Favorite Recipes</a>  
+      <!-- <a2 id="title"> RandomRecipe </a2> -->
+      <a id="login" @click="login">Login</a>
+    </div>  
+    <div class="main">
+      <h1>Welcome to RandomRecipe</h1>
+      <a>RandomRecipe helps take the decision making out of mealtime.  Use our
+        page to find a tasty, health, and easy to make recipies in no time.
+        Our Random button will pick from any reicpie on our list to get
+        you started.  Then, if you log in, you can save and revisit your
+        favorites at any time.  Don't like the randomness, no problem, you
+        can select our All Recipes page to browse our entire selection of
+        top quality recipeis./n</a>
+        <button> RANDOM!!!</button>
+        <h2>https://www.w3schools.com/howto/howto_css_fixed_menu.asp should be cited</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+        <h2>this is to show how it scrolls</h2>
+
+    </div>
   </div>
 </template>
 
@@ -31,4 +61,42 @@ export default class Homepage extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+}
+
+/* Links inside the navbar */
+.navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 16px 16px;
+  text-decoration: none;
+}
+.navbar a2 {
+  float: center;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  font-size: 30px;
+  padding: 8px 8px;
+  text-decoration: none;
+}
+
+
+
+#login {
+  float: right;
+}
+
+/* Change background on mouse-over */
+.navbar a:hover {
+  background: #ddd;
+  color: black;
+}
 </style>
