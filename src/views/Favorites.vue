6339d28cf2daabac0b6bf9f4c7f3563bc55a39bc
alexanderjfang <78180012+alexanderjfang@users.noreply.github.com>
2022-04-19 20:12:32 -0400

    added users favorites to the favorites page, need a remove button

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -9,18 +9,44 @@
       <a v-if="loggedin" id="login" @click="logout">Logout</a>
     </div>
     <h1>Your Favorites</h1>
+    <p v-if="!loggedin">Login to see your favorites!</p>
+     <!-- Below is the table full of recipes -->
+    <table v-if="loggedin">
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Feeds</th>
+        <th>Prep Time</th>
+      </tr>
+      <tr v-for="(u,pos) in userRecipeArray" :key="pos">
+            <td><a @click="singularrecipe(u.id)"><img :src="u.picture" style="width:200px"></a></td>
+            <td><a @click="singularrecipe(u.id)" style="color:blue;text-decoration:underline">{{u.name}}</a></td>
+            <td>{{u.category}}</td>
+            <td>{{u.feeds}}</td>
+            <td>{{u.prepTime}}</td>
+        </tr>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
+import { firebaseConfig } from '@/myconfig';
+import { FirebaseApp, initializeApp } from 'firebase/app';
 import { Auth, getAuth, signOut, User } from 'firebase/auth';
+import { collection, CollectionReference, DocumentData, Firestore, getDocs, getFirestore, QueryDocumentSnapshot, QuerySnapshot } from 'firebase/firestore';
 import { Component, Vue } from 'vue-property-decorator';
+
+const myApp:FirebaseApp = initializeApp(firebaseConfig);
+const myDB:Firestore = getFirestore(myApp);
 
 @Component
 export default class Homepage extends Vue {
   //LOGOUT LOGIC
   auth: Auth | null = null;
   loggedin = false;
+  //create array for storing the recipes
+  userRecipeArray: DocumentData[] = []
   
   homepage(): void{
     this.$router.replace({path: '/'})
@@ -53,6 +79,16 @@ export default class Homepage extends Vue {
     else{
       //user is logged in
       this.loggedin = true
+
+      //grab users data
+      //get collection reference and add recipes to the array
+      let recipes:CollectionReference
+      recipes = collection(myDB, 'userSaves', 'users', uid)
+      getDocs(recipes).then((qs: QuerySnapshot) => {
+        qs.forEach((qd: QueryDocumentSnapshot) => {
+          this.userRecipeArray.push(qd.data())
+        });
+      });
     }
   }
 }
@@ -100,5 +136,21 @@ export default class Homepage extends Vue {
 .navbar a:hover {
   background: #ddd;
   color: black;
+}
+
+table{
+  margin-left: auto;
+  margin-right: auto;
+}
+
+th{
+  min-width: 200px;
+  outline-style: solid;
+  outline-width: 2px;
+}
+
+td{
+  outline-style: solid;
+  outline-width: 2px;
 }
 </style>
